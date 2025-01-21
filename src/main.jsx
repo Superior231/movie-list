5ae105d934c58d76af700fc7ba11c5db85852790
Hikmal Falah <ikmalfalah@gmail.com>
2025-01-21 21:44:59 +0700

    Update {file}: Move the input.css and output.css files to the css folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './pages/App.jsx'
-import './output.css';
+import '../src/assets/css/input.css'
+import '../src/assets/css/output.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
